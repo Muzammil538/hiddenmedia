aa33b1ad897090b2e6a9994f269f4fccbdc5abdc
MAM <70688333+Muzammil538@users.noreply.github.com>
2022-01-30 21:04:27 +0530

    ddd

--- JS/index.js
+++
@@ -1,9 +0,0 @@
-const Form = document.querySelector("#ur-form");
-const Title = document.querySelectorAll(".tag");
-
-Form.addEventListener("submit", ()=>{
-    
-    for(let i =0; i<Title.length ; i++){
-
-    }
-})
